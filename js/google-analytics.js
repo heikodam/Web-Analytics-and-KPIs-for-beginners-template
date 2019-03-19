6514e76572b59508acfda91cf65118556f458e23
Benjamin Bolland <benjamin.bolland@gmail.com>
2019-03-19 15:21:51 +0100

    testrun

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,1 +1,9 @@
 // Put your Google Analytics tracking code here
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136539722-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-136539722-1');
+</script>
