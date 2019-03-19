938a294d268d6fc20f564a190b9e8e33fcb64bef
Benjamin Bolland <benjamin.bolland@gmail.com>
2019-03-19 15:26:19 +0100

    Cleanup

--- js/google-analytics.js
+++
@@ -1,9 +0,0 @@
-// Put your Google Analytics tracking code here
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136539722-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-136539722-1');
-</script>
